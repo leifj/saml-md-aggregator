a0cee27fa527a7e3f7d4af96b39ce74d74cf94b8
Leif Johansson <leifj@sunet.se>
2010-06-19 00:43:50 +0200

    let dispatch handle everything

---
+++ src/main/java/net/nordu/mdx/web/EntityController.java
@@ -0,0 +1,49 @@
+package net.nordu.mdx.web;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import net.nordu.mdx.MetadataException;
+import net.nordu.mdx.index.MetadataIndex;
+import net.nordu.mdx.store.MetadataStore;
+import net.nordu.mdx.utils.MetadataUtils;
+
+import org.oasis.saml.metadata.EntityDescriptorType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/md/*")
+public class EntityController {
+
+	@Autowired
+	private MetadataStore store;
+	@Autowired
+	private MetadataIndex index;
+	
+	@RequestMapping(value="/{tags}", method=RequestMethod.GET)
+	public String list(@PathVariable("tags") String plustags, Model model) throws Exception {
+		String[] tags = plustags.split("\\+");
+		List<EntityDescriptorType> docs = new ArrayList<EntityDescriptorType>();
+		for (String id: index.find(tags)) {
+			System.err.println(id);
+			docs.add(store.load(id));
+		}
+		
+		model.addAttribute("metadataUtils",new MetadataUtils());
+		model.addAttribute("now",new Date());
+		if (docs.size() == 1) {
+			model.addAttribute("entity", docs.get(0));
+			return "entityDetail";
+		} else {
+			model.addAttribute("entities",docs);
+			return "entityList";
+		}
+	}
+	
+}
